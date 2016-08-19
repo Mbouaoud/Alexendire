fec41623f53227e2c98112f8174771493fb74128
Mehdi <bouaoudmehdi@outlook.fr>
2016-08-19 17:32:13 +0200

    Correction de bug

--- src/main/java/org/dta/alexendrie/Run.java
+++ src/main/java/org/dta/alexendrie/Run.java
@@ -3,17 +3,26 @@ package org.dta.alexendrie;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import org.dta.alexendrie.loan.dao.LoanDAO;
+import org.dta.alexendrie.loan.model.Loan;
+import org.dta.alexendrie.media.dao.MediaDAO;
+import org.dta.alexendrie.media.model.Media;
+import org.dta.alexendrie.media.model.MediaType;
 import org.dta.alexendrie.member.dao.MemberDAO;
 import org.dta.alexendrie.member.model.Member;
 
 public class Run {
 	public static void main(String[] args) throws ParseException {
 		
-		SimpleDateFormat date1 = new SimpleDateFormat("yyyy-mm-dd");
-		Member member1 = MemberDAO.creationMember("Aragon", "Romain", "r.aragon@dta.fr", date1.parse("1992-05-12"), "10 rue atoulouse lol", date1.parse("2016-08-19"), 42);
+		SimpleDateFormat date1 = new SimpleDateFormat("yyyy-MM-dd");
+		Member member1 = MemberDAO.creationMember("Angora", "Marion", "r.aragon@dta.fr", date1.parse("1992-05-12"), "10 rue atoulouse lol", date1.parse("2016-08-19"), 42);
+		
+		Media media = MediaDAO.creationMedia("Fight Club", "Chuck Palakniuk", MediaType.Livre);
+		
+		Loan loan1 = LoanDAO.creationEmprunt(member1, media, date1.parse("2016-08-19"));
+		
+		/*EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit");
+		EntityManager em = emf.createEntityManager();*/
+
 	}
 }
--- src/main/java/org/dta/alexendrie/loan/model/Loan.java
+++ src/main/java/org/dta/alexendrie/loan/model/Loan.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 import org.dta.alexendrie.media.model.Media;
@@ -29,6 +31,7 @@ public class Loan {
 
 	@Column
 	@NotNull
+	@Temporal(TemporalType.DATE)
 	private Date dateEmprunt;
 	
 	public Loan(){
