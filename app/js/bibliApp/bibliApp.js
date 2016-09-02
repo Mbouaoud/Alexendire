9b4cd2117ad05c788062db7b11fa28ef067ecd55
Romain Aragon <aragonromain@gmail.com>
2016-09-02 12:16:47 +0200

    modification de index.js et de bibliApps.js

--- app/js/bibliApp/bibliApp.js
+++ app/js/bibliApp/bibliApp.js
@@ -1,3 +1,11 @@
-angular.module('bibliApp', ['ngRoute']).config(function($routeProvider){
+angular
+	.module('bibliApp', ['ngRoute'])
+	
+	.config(function($routeProvider){
+		$routeProvider.when('/home',{
+			templateUrl:'html/bibliApp/media/home.html',
+			controller:'HomeCtrl'
+		});
+
 	
 });
