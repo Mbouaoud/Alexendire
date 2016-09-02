644348642112fbbda6b7f3add9a079c60bf26c09
jim <jim.australia@live.fr>
2016-09-02 15:10:07 +0200

    ajout service authentification

--- app/js/bibliApp/Controllers/login/LoginCtrl.js
+++ app/js/bibliApp/Controllers/login/LoginCtrl.js
@@ -1,8 +1,23 @@
 angular.module('bibliApp')
-.controller('LoginCtrl', function($scope, $location) {
-	$scope.validLogin = function() {
-		if ($scope.formLogin.$valid) {
-			$location.url("/home/" + $scope.user); 
-		}
-	}
+.controller('LoginCtrl', function($scope, $location, Authentification) {
+    this.login = login;
+    (function initController() {
+        Authentication.service.deconnexion();
+    })();
+    function login() {
+        this.dataLoading = true;
+        AuthenticationService.Login(this.login, this.password, function (response) {
+            if (response.success) {
+                Authentication.service.connexion(this.username, this.password);
+                $scope.validLogin = function() {
+            		if ($scope.formLogin.$valid) {
+            			$location.url("/home/" + $scope.user);
+                	} else {
+                		FlashService.Error(response.message);
+                		this.dataLoading = false;
+                	}
+            	};
+        	};
+    	});
+	};	
 });
