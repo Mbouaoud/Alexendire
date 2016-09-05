8442192df4a527ec98ad4b733600e406deacbe73
Romain Aragon <aragonromain@gmail.com>
2016-09-05 17:22:01 +0200

    Ajout de la pagination lors de la visualisation des résultats de la recherche

--- app/js/bibliApp/Controllers/media/HomeCtrl.js
+++ app/js/bibliApp/Controllers/media/HomeCtrl.js
@@ -1,6 +1,7 @@
 angular.module('bibliApp').controller('HomeCtrl', function($scope,$location,MediaRechercheService,$rootScope){
 		
 	$rootScope.typePage='MR';
+	var recherche = [];
 	
 	$scope.mediaType=[{
 		id:1,
@@ -14,19 +15,25 @@ angular.module('bibliApp').controller('HomeCtrl', function($scope,$location,Medi
 	}];
 	
 	$scope.mediaVisualisation = function(){
-		$location.url('/');
+		$location.url('/mediaVisualisation');
 	}
 	
-	$scope.resultMedia = function(){	
-		
+	$scope.resultMedia = function(page){	
 		MediaRechercheService.get($scope.titre,$scope.auteur,$scope.type)
 		.then(function(result){
-			$scope.resultats=result.data;
+			recherche=result.data;
+			console.log(recherche);
+			$scope.resultats=recherche.slice((page-1)*10,page*10);
+			$scope.pageMax = Math.ceil(recherche.length/10);
 		},function(error){
 			this.error=error;
 		});
 
 		$scope.showme=true;
+	}
+	
+	$scope.getNumber = function(num) {
+	    return new Array(num);   
 	}
 	
 })
